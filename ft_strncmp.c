8b52f13e01abf82605f665d6338a33627d703453
tsukasakop <tsukasakop@gmail.com>
2024-04-25 18:46:58 +0900

    bugfix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,33 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkondo <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/19 05:19:24 by tkondo            #+#    #+#             */
-/*   Updated: 2024/01/19 05:19:24 by tkondo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 04:53:19 by tkondo            #+#    #+#             */
-/*   Updated: 2024/01/19 04:53:19 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/04/25 18:45:20 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-
-int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
+#include <stddef.h>
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (n > 0 && (*s1 || *s2))
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
+		if ((*s1 ^ *s2) & 255)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
 		n--;
@@ -37,12 +25,17 @@ int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 	return (0);
 }
 /*
+#include <string.h>
 int main() {
     char str1[] = "Hello";
     char str2[] = "Hell";
-    int result = ft_strncmp(str1, str2, 4);
+str1[2]=127;
+str2[2]=-128;
+    int result = ft_strncmp(str1, str2, -4);
+    int result2 = strncmp(str1, str2, -4);
 
     printf("Comparison result: %d\n", result);
+    printf("Comparison result2: %d\n", result2);
 
     return 0;
 }//*/
