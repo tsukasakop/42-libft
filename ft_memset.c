ab0ad7113d50e683e213c2e9534f27f4bd37e202
tsukasakop <tsukasakop@gmail.com>
2024-04-26 17:02:22 +0900

    ADD: implement ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 16:57:52 by tkondo            #+#    #+#             */
+/*   Updated: 2024/04/26 17:01:57 by tkondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	ch;
+	char			*c_b;
+
+	ch = (unsigned char)c;
+	c_b = (char *)b;
+	while (len)
+	{
+		c_b[len - 1] = ch;
+		len--;
+	}
+	return (b);
+}
