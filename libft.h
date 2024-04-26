6a59c6f1b1db0e2a15a1b62ff103fff54714133e
tsukasakop <tsukasakop@gmail.com>
2024-04-26 17:04:18 +0900

    ADD: insert prototype statement

--- libft.h
+++ libft.h
@@ -1,20 +1,24 @@
 #include <stdbool.h>
 #include <stddef.h>
-bool	ft_isspace(char c);
-int	ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_print_memory(void *addr, unsigned int size);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(char *src);
+
+bool			ft_isspace(char c);
+int				ft_atoi(const char *str);
+void			ft_bzero(void *s, size_t n);
+void			*ft_print_memory(void *addr, unsigned int size);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isprint(int c);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strdup(char *src);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
-size_t	ft_strlen(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, unsigned int n);
-char	*ft_strrchr(const char *s, int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+size_t			ft_strlen(const char *str);
+int				ft_strncmp(const char *s1, const char *s2, unsigned int n);
+char			*ft_strrchr(const char *s, int c);
+int				ft_tolower(int c);
+int				ft_toupper(int c);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
