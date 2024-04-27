662242c2b4f860b70bced0a32f9597e7d70a86d9
tsukasakop <tsukasakop@gmail.com>
2024-04-27 21:19:33 +0900

    ADD: implement ft_lstdelone.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 19:52:48 by tkondo            #+#    #+#             */
-/*   Updated: 2024/04/27 20:52:11 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/04/27 21:13:09 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
