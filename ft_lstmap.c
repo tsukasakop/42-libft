9c0cd8a02774c80ed9ae88cbe8077c0d01a5378f
tsukasakop <tsukasakop@gmail.com>
2024-04-27 23:25:48 +0900

    ADD: implement ft_lstmap.c

---
+++ ft_lstmap.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 22:29:26 by tkondo            #+#    #+#             */
+/*   Updated: 2024/04/27 23:36:17 by tkondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*prev;
+
+	if (lst == NULL)
+		return ;
+	while (*lst)
+	{
+		prev = *lst;
+		*lst = (*lst)->next;
+		ft_lstdelone(prev, del);
+	}
+	*lst = NULL;
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*root;
+	t_list	*new;
+	t_list	*prev;
+	t_list	*cur;
+
+	cur = lst;
+	root = NULL;
+	while (cur)
+	{
+		new = ft_lstnew(f(cur->content));
+		if (new == NULL)
+		{
+			ft_lstclear(&root, del);
+			return (NULL);
+		}
+		if (root == NULL)
+			root = new;
+		else
+			prev->next = new;
+		prev = new;
+		cur = cur->next;
+	}
+	return (root);
+}
+/*
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+static void *str_toupper(void *p)
+{
+    char *str = strdup((char *)p);
+    char *tmp = str;
+    while (*tmp)
+    {
+        *tmp = toupper(*tmp);
+        tmp++;
+    }
+    return (str);
+}
+
+static void *str_tolower(void *p)
+{
+    char *str = strdup((char *)p);
+    char *tmp = str;
+    while (*tmp)
+    {
+        *tmp = tolower(*tmp);
+        tmp++;
+    }
+    return (str);
+}
+int main(){
+    t_list  *lst = ft_lstnew(strdup("Hello"));
+    ft_lstadd_back(&lst, ft_lstnew(strdup(" World")));
+    ft_lstadd_back(&lst, ft_lstnew(strdup(" 42")));
+    ft_lstadd_back(&lst, ft_lstnew(strdup(" Tokyo!")));
+    ft_lstadd_back(&lst, ft_lstnew(strdup("")));
+
+    // toupper
+    t_list *newlst = ft_lstmap(lst, str_toupper, free);
+    // tolower
+    newlst = ft_lstmap(lst, str_tolower, free);
+	while(newlst)
+{
+printf("%s\n", newlst->content);
+newlst=newlst->next;}
+}//*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 19:52:48 by tkondo            #+#    #+#             */
-/*   Updated: 2024/04/27 22:06:09 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/04/27 22:11:27 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,7 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 #endif
