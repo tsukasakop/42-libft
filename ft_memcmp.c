ade200d5812219f50586ec32d8193ebb2109932d
tsukasakop <tsukasakop@gmail.com>
2024-04-27 11:49:22 +0900

    ADD: implement ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 11:46:06 by tkondo            #+#    #+#             */
+/*   Updated: 2024/04/27 11:49:10 by tkondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*c1;
+	const unsigned char	*c2;
+	size_t				i;
+
+	c1 = (const unsigned char *)s1;
+	c2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (c1[i] != c2[i])
+			return (int)(c1[i] - c2[i]);
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -24,3 +24,4 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
