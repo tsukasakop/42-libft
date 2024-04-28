3eadd4da1e053d54225aa853e0bc448ade4a3ad4
tsukasakop <tsukasakop@gmail.com>
2024-04-28 21:24:37 +0900

    refactor

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,31 +6,24 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 05:07:23 by tkondo            #+#    #+#             */
-/*   Updated: 2024/04/28 18:22:00 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/04/28 21:24:12 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdlib.h>
 
 char	*ft_strdup(const char *s1)
 {
 	int		len;
-	char	*dup;
-	int		i;
+	char	*p;
 
-	len = 0;
-	while (s1[len] != '\0')
-		len++;
-	dup = malloc(len + 1);
-	if (dup == NULL)
+	len = ft_strlen(s1);
+	p = malloc(len + 1);
+	if (p == NULL)
 		return (NULL);
-	i = 0;
-	while (i <= len)
-	{
-		dup[i] = s1[i];
-		i++;
-	}
-	return (dup);
+	ft_memcpy(p, s1, len + 1);
+	return (p);
 }
 
 /*
