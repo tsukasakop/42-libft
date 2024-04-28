761a5b9743aed3399c0a74530cc00edc48c58434
tsukasakop <tsukasakop@gmail.com>
2024-04-28 20:43:32 +0900

    bugfix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,38 +6,30 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 04:16:43 by tkondo            #+#    #+#             */
-/*   Updated: 2024/04/28 18:29:04 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/04/28 20:37:10 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stddef.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	unsigned int	dst_len;
-	unsigned int	src_len;
-	unsigned int	i;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	cpy_len;
 
-	dst_len = 0;
-	src_len = 0;
-	i = 0;
-	while (dst && dst[dst_len] != '\0')
-		dst_len++;
-	while (src[src_len] != '\0')
-		src_len++;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
 	if (dst_len >= dstsize)
 		return (src_len + dstsize);
-	if (dst_len < dstsize - 1)
-	{
-		while (src[i] != '\0' && dst_len < dstsize - 1)
-		{
-			dst[dst_len] = src[i];
-			dst_len++;
-			i++;
-		}
-		dst[dst_len] = '\0';
-	}
-	return (src_len + dst_len - i);
+	if (dstsize > dst_len + src_len)
+		cpy_len = src_len;
+	else
+		cpy_len = dstsize - dst_len - 1;
+	ft_memmove(dst + dst_len, src, cpy_len);
+	dst[dst_len + cpy_len] = '\0';
+	return (src_len + dst_len);
 }
 /* RM this line
 #include <stdio.h>
