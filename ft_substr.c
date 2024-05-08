5285d65ebc5cfe273c8b02b9cf4bdc6971266943
tsukasakop <tsukasakop@gmail.com>
2024-05-09 08:06:53 +0900

    norm

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 14:39:36 by tkondo            #+#    #+#             */
-/*   Updated: 2024/05/09 07:57:37 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/05/09 08:06:22 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	n;
 	char	*p;
 
-	if(s == NULL)
-		return NULL;
+	if (s == NULL)
+		return (NULL);
 	n = ft_strlen(s);
-	if(start)
+	if (start)
 	{
 		if (n > start)
 			n = start;
-		return ft_substr(s+n, 0, len);
+		return (ft_substr(s + n, 0, len));
 	}
-	if(len < n)
+	if (len < n)
 		n = len;
 	p = malloc(n + 1);
 	if (p == NULL)
 		return (NULL);
-	ft_memmove(p,s,n);
+	ft_memmove(p, s, n);
 	p[n] = '\0';
 	return (p);
 }
