3ffd5a722553c815f6419562a8e3bde5871280fa
tsukasakop <tsukasakop@gmail.com>
2024-05-09 01:05:49 +0900

    refactor

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,35 +6,31 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 10:27:57 by tkondo            #+#    #+#             */
-/*   Updated: 2024/04/27 19:08:09 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/05/09 01:05:26 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <limits.h>
-#include <stdbool.h>
-#include <stdio.h>
 
 int	ft_atoi(const char *str)
 {
-	int		cur;
 	char	sign;
 	long	num;
 	long	limit;
 
-	cur = 0;
-	while ((str[cur] >= 9 && str[cur] <= 13) || str[cur] == ' ')
-		cur++;
-	sign = 1 - 2 * (str[cur] == '-');
-	cur += (str[cur] == '+' || str[cur] == '-');
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	sign = 1 - 2 * (*str == '-');
+	str += *str == '+' || *str == '-';
 	limit = LONG_MIN + (sign + 1) / 2;
 	num = 0;
-	while (ft_isdigit(str[cur]))
+	while (ft_isdigit(*str))
 	{
-		if (sign * -1 * num < (limit + str[cur] - '0') / 10)
+		if (sign * -1 * num < (limit + *str - '0') / 10)
 			return ((sign + 1) * -1 / 2);
-		num = num * 10 + sign * (str[cur] - '0');
-		cur++;
+		num = num * 10 + sign * (*str - '0');
+		str++;
 	}
 	return (num & -1);
 }
