15422961a3907267bc832a784be21faa2cbf7a7f
tsukasakop <tsukasakop@gmail.com>
2024-05-09 06:29:04 +0900

    norm

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 10:27:57 by tkondo            #+#    #+#             */
-/*   Updated: 2024/05/09 04:58:31 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/05/09 06:21:45 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_atoi(const char *str)
 	while ((*str >= 9 && *str <= 13) || *str == ' ')
 		str++;
 	sign = 1 - 2 * (*str == '-');
-	str += *str == '+' || *str == '-';
+	str += (*str == '+' || *str == '-');
 	limit = LONG_MIN + (sign + 1) / 2;
 	num = 0;
 	while (ft_isdigit(*str))
