1328966298d3679c3fb0b2f6fa5f422808cf67bc
tsukasakop <tsukasakop@gmail.com>
2024-05-13 16:39:38 +0900

    rollback lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,34 +6,36 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 22:29:26 by tkondo            #+#    #+#             */
-/*   Updated: 2024/05/11 18:47:13 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/05/13 16:04:32 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*node;
-	t_list	*leaf;
-	void	*p;
+	t_list	*root;
+	t_list	*new;
+	t_list	*prev;
+	t_list	*cur;
 
-	if (lst == NULL || f == NULL || del == NULL)
-		return (NULL);
-	p = f(lst->content);
-	if (p == NULL)
-		return (NULL);
-	node = ft_lstnew(p);
-	if (node == NULL)
-		return (NULL);
-	if (lst->next == NULL)
-		return (node);
-	leaf = ft_lstmap(lst->next, f, del);
-	if (leaf == NULL)
+	cur = lst;
+	root = NULL;
+	while (cur)
 	{
-		ft_lstdelone(node, del);
-		return (NULL);
+		new = ft_lstnew(f(cur->content));
+		if (new == NULL)
+		{
+			ft_lstclear(&root, del);
+			return (NULL);
+		}
+		if (root == NULL)
+			root = new;
+		else
+			prev->next = new;
+		prev = new;
+		cur = cur->next;
 	}
-	ft_lstadd_front(&leaf, node);
-	return (leaf);
+	return (root);
 }
