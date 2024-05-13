4605fab604b42d196ae568cbc5f34c4011193d35
tsukasakop <tsukasakop@gmail.com>
2024-05-13 16:00:10 +0900

    bugfix

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 13:33:35 by tkondo            #+#    #+#             */
-/*   Updated: 2024/05/11 18:16:29 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/05/13 15:48:16 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static bool	cnt_word(char const *begin, char const *end, void *p)
 {
 	(void)begin;
 	(void)end;
-	(*((int *)p))++;
+	(**((int **)p))++;
 	return (true);
 }
 
@@ -50,7 +50,7 @@ static bool	split_word_iter(char const *s, char c, void *p,
 		cur_end = ft_strchr(cur_begin, c);
 		if (cur_end == NULL)
 			cur_end = cur_begin + ft_strlen(cur_begin);
-		if (!f(cur_begin, cur_end, p))
+		if (!f(cur_begin, cur_end, &p))
 			return (false);
 		cur_begin = cur_end;
 	}
@@ -58,30 +58,31 @@ static bool	split_word_iter(char const *s, char c, void *p,
 
 static void	free_all(char ***p)
 {
-	char	***begin;
+	char	**begin;
 
-	begin = p;
+	if (p == NULL || *p == NULL)
+		return ;
+	begin = *p;
 	while (**p)
 	{
 		free(**p);
 		(*p)++;
 	}
-	free(*begin);
+	free(begin);
+	*p = NULL;
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**p;
 	int		cnt;
-	char	**_p;
 
 	if (s == NULL)
 		return (NULL);
 	cnt = 0;
 	split_word_iter(s, c, &cnt, cnt_word);
 	p = ft_calloc(sizeof(char *), cnt + 1);
-	_p = p;
-	if (p != NULL && !split_word_iter(s, c, &_p, append_word))
+	if (p != NULL && !split_word_iter(s, c, p, append_word))
 		free_all(&p);
 	return (p);
 }
