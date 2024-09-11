9e6a64883ad1fd05d097adc04c714bcc3c905c42
tsukasakop <tsukasakop@gmail.com>
2024-09-11 11:33:21 +0900

    ✨ Add fputc

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/09 00:35:59 by tkondo            #+#    #+#              #
-#    Updated: 2024/09/11 11:27:43 by tkondo           ###   ########.fr        #
+#    Updated: 2024/09/11 11:32:26 by tkondo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -64,6 +64,7 @@ TARGET =\
 	ft_lstmap\
 	ft_fileno\
 	ft_fwrite\
+	ft_fputc\
 
 OBJS = $(addprefix $(OBJ_DIR)/,$(addsuffix .o,$(TARGET)))
 
--- src/ft_fputc.c
+++ src/ft_fputc.c
@@ -1,26 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_stdio.h                                         :+:      :+:    :+:   */
+/*   ft_fputc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/27 19:52:48 by tkondo            #+#    #+#             */
-/*   Updated: 2024/09/11 11:30:36 by tkondo           ###   ########.fr       */
+/*   Created: 2024/07/24 12:19:13 by tkondo            #+#    #+#             */
+/*   Updated: 2024/09/11 11:22:36 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_STDIO_H
-# define FT_STDIO_H
+#include "ft_stdio.h"
 
-# include <stdio.h>
+int	ft_fputc(int c, FILE *stream)
+{
+	unsigned char	uc;
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-int		ft_fileno(FILE *stream);
-size_t	ft_fwrite(const void *ptr, size_t size, size_t n_items, FILE *stream);
-int		ft_fputc(int c, FILE *stream);
-
-#endif
+	uc = (unsigned char)c;
+	return (ft_fwrite((const char *)&uc, 1, 1, stream) * 2 - 1);
+}
