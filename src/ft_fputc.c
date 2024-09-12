4c78cd95e8f8f9865b9bd74b95d0e1943ee9dd54
tsukasakop <tsukasakop@gmail.com>
2024-09-12 21:25:48 +0900

    ✨ Use buffer on fputc for macOS

--- src/ft_fputc.c
+++ src/ft_fputc.c
@@ -6,12 +6,38 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 12:19:13 by tkondo            #+#    #+#             */
-/*   Updated: 2024/09/11 23:08:30 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/09/12 20:32:20 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_stdio.h"
-#include <unistd.h>
+#ifdef __APPLE__
+# include "ft_stdio.h"
+# include "ft_stdlib.h"
+
+int	ft_fputc(int c, FILE *stream)
+{
+	size_t	bfsize;
+
+	bfsize = 4096;
+	if (stream->_bf._base == NULL)
+	{
+		stream->_bf._base = (unsigned char *)ft_calloc(bfsize, 1);
+		stream->_bf._size = bfsize;
+		if (stream->_bf._base == NULL)
+			return (EOF);
+		stream->_p = stream->_bf._base;
+	}
+	if (stream->_w + stream->_bf._size == 0 && ft_fflush(stream) == EOF)
+		return (EOF);
+	*stream->_p = (unsigned char)c;
+	stream->_p++;
+	stream->_w--;
+	return (c);
+}
+
+#elif __linux__
+# include "ft_stdio.h"
+# include <unistd.h>
 
 int	ft_fputc(int c, FILE *stream)
 {
@@ -19,3 +45,5 @@ int	ft_fputc(int c, FILE *stream)
 		return (EOF);
 	return (c);
 }
+
+#endif
