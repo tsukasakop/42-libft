ccff8bedd31c0699bbbb6b16fd85cfb299159c64
Tsukasa Kondo <tsukasakop@MacBook-Air-2018.local>
2024-12-11 15:26:09 +0900

    ✏️ Implement global management functions

---
+++ include/ft_global.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_global.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 14:58:57 by tkondo            #+#    #+#             */
+/*   Updated: 2024/12/11 15:27:34 by tkondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GLOBAL_H
+# define FT_GLOBAL_H
+
+# include "ft_htbl.h"
+# include "ft_memory.h"
+
+ft_htbl	ft_get_globals(void);
+int		ft_set_global(const char *key, void *val);
+void	*ft_get_global(const char *key);
+
+#endif
