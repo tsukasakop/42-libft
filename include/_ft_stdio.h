9c9146d86068505b7a0f3d2d2aac646001542b5e
Tsukasa Kondo <tsukasakop@MacBook-Air-2018.local>
2024-12-17 11:14:29 +0900

    🏷️ Add struct for vfprintf process

--- include/_ft_stdio.h
+++ include/_ft_stdio.h
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 03:11:49 by tkondo            #+#    #+#             */
-/*   Updated: 2024/12/16 18:00:01 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/12/17 10:33:13 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 # include "ft_stdio.h"
 # include "libft.h"
 
-typedef enum e_print_flag
+typedef enum e_print_flag t_print_flag;
+typedef struct s_format t_format;
+typedef struct s_print t_print;
+
+enum e_print_flag
 {
 	PAD_ZERO = 1,
 	ADJUST_LEFT = 2,
@@ -23,16 +27,28 @@ typedef enum e_print_flag
 	SIGN_NUM = 8,
 	ALTER_FORM = 16,
 	PRECITION = 32,
-}					t_print_flag;
+};
 
-typedef struct t_print
+struct s_format
 {
 	unsigned char	mod;
-//	char			**orig;
-	char			*cur;
+	const char			*begin;
+	const char			**orig;
+	const char			*cur;
 	void			*val;
 	int				opt[3];
-	FILE			*s;
-	int				(*print_val)(struct t_print *, int);
-}					s_print;
+};
+
+struct s_print
+{
+    char sign;
+   const char *prefix;
+    int l_ws_len;
+    int zero_len;
+    int inner_len;
+    int r_ws_len;
+    const char *base;
+    const unsigned char *p;
+};
+
 #endif
